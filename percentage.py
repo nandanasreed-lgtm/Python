4328f1a610510d739802e478637593ea0fa172fe
nandanasreed-lgtm <nandanasreed@gmail.com>
2026-04-27 20:23:03 +0100

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,9 @@
+print("Enter your marks out of 100 :")
+m=int(input("Maths :"))
+e=int(input("English :"))
+s=int(input("Science :"))
+
+sum=m+e+s
+print("Total marks obtained =" ,sum)
+p=(sum/300)*100
+print("Percentage: ",p)
