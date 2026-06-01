bf6bd003636a6a4721bce1323e94ceb9589a0fcd
nandanasreed-lgtm <nandanasreed@gmail.com>
2026-06-01 20:15:55 +0100

    Add files via upload

---
+++ operatorpriorty.py
@@ -0,0 +1,13 @@
+v = 2
+w = 4
+x = 6
+y = 8
+z = (v+w)*x/y
+print("z = ",z)
+name = "Bob"
+age = 0
+if name == "Bob" or name == "John" and age >= 2:
+    print("Hello welcome")
+else:
+    print("Goodbye")
+          
