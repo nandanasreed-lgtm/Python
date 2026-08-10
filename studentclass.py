bdfae55b24404ab927b0460336bb8401a4a08c05
nandanasreed-lgtm <nandanasreed@gmail.com>
2026-08-10 19:55:19 +0100

    Add files via upload

---
+++ studentclass.py
@@ -0,0 +1,8 @@
+# write a program to create a class with a name Student and perform the following tasks:
+#1 declare a variable grade
+#2 print a sentence inside the class
+#3 create an object of class Student
+class Student:
+    grade = 7
+    print("Hi! I am a student of grade : ",grade)
+ob = Student()
